db19da56d0c86c1da7d117cc6a6496375f97f54c
TheDJ45 <s062969@ad.viko.lt>
2025-09-18 09:52:36 +0300

    uzduotys

---
+++ komanda.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ int antras() {
+    string name = "Žalgiris";
+    int metai = 1944;
+    string sav = "Paulius Jankūnas";
+    string arena = "Žalgirio arena";
+    int vietos = 15500;
+    cout << "Krepsinio komanda: " << name << "\n" << "Ikurimo metai: " << metai << "\n" << "Savininkas: " << sav << "\n" << "Arena: " << arena << "\n" << "Vietu skaicius" << vietos << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ int main() {
+     string a = "Dominykas";
+     string b = "Jasevicius";
+     int c = 20;
+     string d = "PI25";
+     string e = "strukturinis programavimas";
+     string f = "Programu sistemos";
+     cout << " Vardas: " << a << "\n" << " Pavarde: " << b << "\n" << " Amzius: " << c << "\n" << " Grupe: " << d << "\n" << " Kursas: " << e << "\n" << " Studiju programa: " << f << "\n" << endl;
+     return 0;
+ }
